8872c5dde1ee2355a59171be210dc028a336fa7b
carreldhaiti <carreldhaiti@yahoo.com>
2018-09-11 13:45:25 -0400

    Bitcoin ticker project has been completed

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,6 +12,14 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestHandle;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    // Complete url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -45,35 +54,88 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener( new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
 
-    }
+                // An item was selected. You can retrieve it using adapterView.getItemAtPosition( pos )
+                Log.d( "Bitcoin", "Dropdown value: " + ( adapterView.getItemAtPosition(position) ) );
+
+                String strURL = BASE_URL + adapterView.getItemAtPosition( position );
+
+                // Invoke the private method letsDoSomeNetworking(String url)
+                letsDoSomeNetworking( strURL );
+
+            }   // end of onItemSelected() method
+
+            @Override
+            public void onNothingSelected( AdapterView<?> parent ) {
+
+                Log.d( "Bitcoin", "Nothing Selected" );
+
+            }   // end of method onNothingSelected()
+
+        }); // end of method setOnItemSelectedListener()
+
+    }   // end of method onCreate()
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                // On success, the response HTTP status is "200 OK"
+                Log.d( "Bitcoin", "status code: " + statusCode );
+
+                // Show the JSON string from the Bitcoin site
+                Log.d("Bitcoin", "JSON: " + response.toString());
+
+                // Extract the requested information from the JSON response
+                try {
+
+                    // extract ask price from JSON
+                    String strAskPriceFromJson = response.getString( "ask" );
+                    Log.d("Bitcoin", "JSON ask price: " + strAskPriceFromJson );
+
+                    // extract bid price from JSON
+                    String strBidPriceFromJson = response.getString( "bid" );
+                    Log.d("Bitcoin", "JSON bid price: " + strBidPriceFromJson );
+
+                    // extract last price from JSON
+                    String strLastPriceFromJson = response.getString( "last" );
+                    Log.d("Bitcoin", "JSON last price: " + strLastPriceFromJson );
+
+                    // extract day average price from JSON
+                    String strDayAvgPriceFromJson = ( Double.toString( response.getJSONObject("averages").getDouble("day" ) ) );
+                    Log.d("Bitcoin", "JSON day average price: " + strDayAvgPriceFromJson );
+
+                    // Update the mPriceTextView with the last Bitcoin price information from the JSON
+                    mPriceTextView.setText( strLastPriceFromJson );
+
+                } catch( JSONException e ) {
+
+                    Log.d("Bitcoin", "JSON error in extracting the Bitcoin price: " + e.toString() ) ;
+
+                }   // end of try-catch block
+
+            }   // end of method onSuccess()
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+
+            }   // end of method onFailure()
+
+        }); // end of method get()
+
+    }   // end of method letsDoSomeNetworking()
 
 
-    }
-
-
-}
+}   // end of class MainActivity
